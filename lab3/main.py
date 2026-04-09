2c560a05dbfae0010ebff73ab90944df89a11c99
someotb <llyryk934@gmail.com>
2026-04-09 13:50:39 +0700

    feat(lab3): add tasks 1, 2

---
+++ lab3/main.py
@@ -0,0 +1,67 @@
+from cProfile import label
+from multiprocessing import allow_connection_pickling
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def get_pdf(mean, var, x_array):
+    return np.exp(-((x_array - mean) ** 2) / (2 * var)) / np.sqrt(2 * np.pi * var)
+
+
+def get_empirical_pdf(xn, bins, step):
+    counts = np.zeros(len(bins) - 1)
+
+    for value in xn:
+        for i in range(len(bins) - 1):
+            if bins[i] <= value < bins[i + 1]:
+                counts[i] += 1
+                break
+
+    empirical_pdf = counts / (len(xn) * step)
+    return empirical_pdf
+
+
+# Task 1
+mean_0 = 0
+mean_m1 = -1
+var_1 = 1
+var_3 = 3
+var_02 = 0.2
+w_0_1 = []
+w_0_3 = []
+w_0_02 = []
+w_m1_1 = []
+x_array = np.arange(-5, 5, 0.01)
+
+w_0_1 = get_pdf(mean_0, var_1, x_array)
+w_0_3 = get_pdf(mean_0, var_3, x_array)
+w_0_02 = get_pdf(mean_0, var_02, x_array)
+w_m1_1 = get_pdf(mean_m1, var_1, x_array)
+
+plt.figure(1, label="График плотности распределения")
+plt.title("График плотности распределения")
+plt.plot(x_array, w_0_1, color="r", label=f"mx = {mean_0}, var = {var_1}")
+plt.plot(x_array, w_0_3, color="b", label=f"mx = {mean_0}, var = {var_3}")
+plt.plot(x_array, w_0_02, color="y", label=f"mx = {mean_0}, var = {var_02}")
+plt.plot(x_array, w_m1_1, color="g", label=f"mx = {mean_m1}, var = {var_1}")
+plt.legend()
+
+# Task 2
+t = np.linspace(0, 3, 1000)
+s1 = np.sqrt(var_1)
+s3 = np.sqrt(var_3)
+s02 = np.sqrt(var_02)
+
+xn_1 = np.random.normal(mean_0, s1, len(t))
+xn_3 = np.random.normal(mean_0, s3, len(t))
+xn_02 = np.random.normal(mean_0, s02, len(t))
+
+plt.figure(2, label="Вектор значений СВ с нормальным распределением")
+plt.title("Вектор значений СВ")
+plt.scatter(t, xn_1, s=1, color="r", label=f"mx = {mean_0}, s = {s1}")
+plt.scatter(t, xn_3, s=1, color="b", label=f"mx = {mean_0}, s = {s3}")
+plt.scatter(t, xn_02, s=1, color="y", label=f"mx = {mean_0}, s = {s02}")
+plt.legend()
+
+plt.show()
