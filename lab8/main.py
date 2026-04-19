ada5afb6a32aa6b53b5b24f024cee0197a5f7fc1
someotb <llyryk934@gmail.com>
2026-04-19 10:14:49 +0700

    feat(lab8): complite lab8

--- lab8/main.py
+++ lab8/main.py
@@ -1,11 +1,16 @@
-from matplotlib import pyplot as plt
-import numpy as np
 import random
+from cProfile import label
+
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 def qpsk_mapper(bits: list):
     symbols = np.zeros(len(bits) // 2, dtype=complex)
     for i in range(len(bits) // 2):
-        symbols[i] = (complex(1.0 - 2.0 * bits[2 * i + 0], 1.0 - 2.0 * bits[2 * i + 1]) / np.sqrt(2.0)) 
+        symbols[i] = complex(
+            1.0 - 2.0 * bits[2 * i + 0], 1.0 - 2.0 * bits[2 * i + 1]
+        ) / np.sqrt(2.0)
     return symbols
 
 
@@ -15,8 +20,10 @@ def upsampler(symbols, upsample_koef):
         up_symbols[i * upsample_koef] = symbols[i]
     return up_symbols
 
+
 def downsample(symbols, upsample_koef):
     return symbols[::upsample_koef]
+
 
 def white_noise(conv_symbols, snr_db):
     cos2 = np.abs(conv_symbols) ** 2
@@ -26,37 +33,46 @@ def white_noise(conv_symbols, snr_db):
     noise_awg_watts = 10 ** (noise_awg_db / 10)
     mean_noise = 0
 
-
-    noise_volts_re = np.random.normal(mean_noise, np.sqrt(noise_awg_watts), len(conv_symbols))
-    noise_volts_im = np.random.normal(mean_noise, np.sqrt(noise_awg_watts), len(conv_symbols))
+    noise_volts_re = np.random.normal(
+        mean_noise, np.sqrt(noise_awg_watts), len(conv_symbols)
+    )
+    noise_volts_im = np.random.normal(
+        mean_noise, np.sqrt(noise_awg_watts), len(conv_symbols)
+    )
     n_compl = noise_volts_re + 1j * noise_volts_im
     cs_noisy = conv_symbols + n_compl
     return cs_noisy
 
+
 def demapper(symbols):
-    demapped_bits = np.zeros(len(symbols), dtype=list)
+    demapped_bits = np.zeros(len(symbols) * 2, dtype=int)
     kor = np.sqrt(2.0)
-    znach = {(1 + 1j / kor): (0, 0),
-             (1 - 1j / kor): (0, 1),
-             (-1 - 1j / kor): (1, 1),
-             (-1 + 1j / kor): (1, 0)}
-    
-    for i in range(len(symbols) // 2):
-        min_ras = 10
-        symb = 0 + 1j
+    znach = {
+        ((1 + 1j) / kor): (0, 0),
+        ((1 - 1j) / kor): (0, 1),
+        ((-1 - 1j) / kor): (1, 1),
+        ((-1 + 1j) / kor): (1, 0),
+    }
+
+    for i in range(len(symbols)):
+        min_ras = float("inf")
+        best_bits = (0, 0)
         for k, z in znach.items():
-            rast = np.sqrt((symbols[i].real - k.real)**2 + (symbols[i].imag - k.imag)**2)
+            rast = np.sqrt(
+                (symbols[i].real - k.real) ** 2 + (symbols[i].imag - k.imag) ** 2
+            )
             if rast < min_ras:
                 min_ras = rast
-                symb = z
-        demapped_bits[2 * i] = symb[0]
-        demapped_bits[2 * i + 1] = symb[1]
+                best_bits = z
+
+        demapped_bits[2 * i] = best_bits[0]
+        demapped_bits[2 * i + 1] = best_bits[1]
     return demapped_bits
 
-        
+
 bits = []
 up_koef = 10
-snr_db = 10
+snr_db = 3
 ih = np.ones(up_koef)
 
 for i in range(1000):
@@ -65,62 +81,64 @@ for i in range(1000):
 # TX
 symbols = qpsk_mapper(bits)
 up_symbols = upsampler(symbols, up_koef)
-conv_symbols = np.convolve(up_symbols, ih)
+conv_symbols = np.convolve(up_symbols, ih, mode="same")
 symbols_n = white_noise(conv_symbols, snr_db)
 
 # RX
-rx_symbols_conv = np.convolve(symbols_n, ih) / up_koef
+rx_symbols_conv = np.convolve(symbols_n, ih, mode="same") / up_koef
 rx_down_symbols = downsample(rx_symbols_conv, up_koef)
 dem_symb = demapper(rx_down_symbols)
 
-plt.figure(1)
+plt.figure(1, label="Bits")
 plt.plot(bits)
 
-plt.figure(2)
-plt.subplot(2,1,1)
+plt.figure(2, label="Symbols")
+plt.subplot(2, 1, 1)
 plt.plot(np.real(symbols))
 plt.plot(np.imag(symbols))
-plt.subplot(2,1,2)
-plt.scatter(symbols.real, symbols.imag)
+plt.subplot(2, 1, 2)
+plt.scatter(np.real(symbols), np.imag(symbols))
 
 
-plt.figure(3)
-plt.subplot(2,1,1)
+plt.figure(3, label="UpSample symbols")
+plt.subplot(2, 1, 1)
 plt.plot(np.real(up_symbols))
 plt.plot(np.imag(up_symbols))
-plt.subplot(2,1,2)
-plt.scatter(up_symbols.real, up_symbols.imag)
+plt.subplot(2, 1, 2)
+plt.scatter(np.real(up_symbols), np.imag(up_symbols))
 
 
-plt.figure(4)
-plt.subplot(2,1,1)
+plt.figure(4, label="Symbols after Convolve")
+plt.subplot(2, 1, 1)
 plt.plot(np.real(conv_symbols))
 plt.plot(np.imag(conv_symbols))
-plt.subplot(2,1,2)
-plt.scatter(conv_symbols.real, conv_symbols.imag)
+plt.subplot(2, 1, 2)
+plt.scatter(np.real(conv_symbols), np.imag(conv_symbols))
 
-plt.figure(5)
-plt.subplot(2,1,1)
+plt.figure(5, label="Noisy Symbols")
+plt.subplot(2, 1, 1)
 plt.plot(np.real(symbols_n))
 plt.plot(np.imag(symbols_n))
-plt.subplot(2,1,2)
-plt.scatter(symbols_n.real, symbols_n.imag)
+plt.subplot(2, 1, 2)
+plt.scatter(np.real(symbols_n), np.imag(symbols_n))
 
-plt.figure(6)
-plt.subplot(2,1,1)
+plt.figure(6, label="RX symbols")
+plt.subplot(2, 1, 1)
 plt.plot(np.real(rx_symbols_conv))
 plt.plot(np.imag(rx_symbols_conv))
-plt.subplot(2,1,2)
-plt.scatter(rx_symbols_conv.real, rx_symbols_conv.imag)
+plt.subplot(2, 1, 2)
+plt.scatter(np.real(rx_symbols_conv), np.imag(rx_symbols_conv))
 
-plt.figure(7)
-plt.subplot(2,1,1)
+plt.figure(7, label="RX Symbols after down sampling")
+plt.subplot(2, 1, 1)
 plt.plot(np.real(rx_down_symbols))
 plt.plot(np.imag(rx_down_symbols))
-plt.subplot(2,1,2)
-plt.scatter(rx_down_symbols.real, rx_down_symbols.imag)
+plt.subplot(2, 1, 2)
+plt.scatter(np.real(rx_down_symbols), np.imag(rx_down_symbols))
 
-plt.figure(8)
-plt.plot(dem_symb)
+plt.figure(8, label="Decoded Bits vs Bits")
+plt.plot(dem_symb, label="Dec bits")
+plt.plot(bits, label="bits")
+plt.legend()
 
 plt.show()
