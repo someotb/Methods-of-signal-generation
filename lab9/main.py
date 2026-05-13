eb1eba03f979c9c0fd0634288886badd25e1e91b
someotb <llyryk934@gmail.com>
2026-05-13 23:23:15 +0700

    feat(lab9): complite lab9

--- lab9/main.py
+++ lab9/main.py
@@ -2,15 +2,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.linalg import convolution_matrix, inv
 
-def base_signal_output(signal:complex, label:str):
-    plt.figure(figsize=(8,8), label=label)
-    plt.subplot(2,1,1)
+
+def base_signal_output(signal: complex, label: str):
+    plt.figure(figsize=(8, 8), label=label)
+    plt.subplot(2, 1, 1)
     plt.plot(signal.real, label="I")
     plt.plot(signal.imag, label="Q")
-    plt.subplot(2,1,2)
+    plt.subplot(2, 1, 2)
     plt.scatter(signal.real, signal.imag, label="I/Q")
 
-def modulator(mod_type:str, bits):
+
+def modulator(mod_type: str, bits):
     match mod_type:
         case "BPSK":
             symbols = np.zeros(len(bits), dtype=complex)
@@ -29,6 +31,7 @@ def modulator(mod_type:str, bits):
         case _:
             return "Unsupported modulation type"
 
+
 def white_noise(symbols, snr_db):
     cos2 = np.abs(symbols) ** 2
     sig_awg_watts = np.mean(cos2)
@@ -45,23 +48,25 @@ def white_noise(symbols, snr_db):
     )
     n_compl = noise_volts_re + 1j * noise_volts_im
     cs_noisy = symbols + n_compl
-    return cs_noisy          
+    return cs_noisy
+
 
 SNR = 18
 ones = np.ones(10)
-h = [0.19 + 1j * 0.56, 0.45 - 1j * 1.28, -0.14 - 1j * 0.53]
-tr = [0,1,0,0,0,1,1,1,1,0,1,1,0,1,0,0,0,1,0,0,0,1,1,1,1,0]
+h = np.array([0.19 + 1j * 0.56, 0.45 - 1j * 1.28, -0.14 - 1j * 0.53])
+tr = [0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0]
 offset = (len(tr) - 16) // 2
 tr_central = tr[offset : len(tr) - offset]
 bits = np.random.randint(0, 2, 1000)
 symbols = modulator("QPSK", bits)
-conv_symb = np.convolve(symbols, 1, mode="same")
+conv_symb = np.convolve(symbols, h[0:1], mode="same")
 n_symb = white_noise(conv_symb, SNR)
 rec_symb = np.convolve(n_symb, h[0:3], mode="same")
 tr_bpsk = modulator("BPSK", tr_central)
 akf_tr_bpsk = np.correlate(tr_bpsk, tr_bpsk, mode="same")
-M = convolution_matrix(tr_central, len(tr_central))
-hLS = inv(np.conj(M.T) @ M) @ np.conj(M.T) @ tr_bpsk
+r = np.convolve(tr_bpsk, h, mode="same")
+M = convolution_matrix(tr_bpsk, len(h), mode="same")
+hLS = inv(np.conj(M.T) @ M) @ np.conj(M.T) @ r
 
 print(h)
 print(hLS)
@@ -73,9 +78,7 @@ plt.stem([h[i].real for i in range(len(h))])
 base_signal_output(rec_symb, "Rec Symbols")
 base_signal_output(tr_bpsk, "Tr BPSK")
 
-plt.figure(figsize=(8,8))
+plt.figure(figsize=(8, 8))
 plt.plot(np.abs(akf_tr_bpsk), label="AKF")
-
-
 
 plt.show()
